b0caf05cec3da8172d57600798f48ac21c6e5e4b
Thủy Đào <daoduythuy@gmail.com>
2024-11-08 11:31:32 +0700

    Update openai.js

--- trash1/openai.js
+++ trash1/openai.js
@@ -1,4 +1,8 @@
+const axios = require("axios");
 const sendUpdate = async (...args) => {
   console.log("Received params:", args);
+  const [url] = args
+  const response = await axios.get(url)
+  return response.data || null
   // Thực hiện các xử lý khác với params
 };
